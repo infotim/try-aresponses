25a0d80e197516edecaeb7ecc07aa0cffb408f04
Timofey Stolbov <timofey@stolbov.info>
2020-06-04 13:44:44 +0500

    Show multiple response mocking

--- service/service_test.py
+++ service/service_test.py
@@ -5,6 +5,16 @@ from .service import Service
 
 pytestmark = pytest.mark.filterwarnings("ignore::DeprecationWarning")
 
+@pytest.fixture
+def mock_multiple_responses(aresponses):
+    # Add more if needed :)
+    aresponses.add(
+        "httpbin.org",
+        "/get",
+        "GET",
+        response=aiohttp.web.json_response({"origin":"127.0.0.1"})
+    )
+
 @pytest.mark.asyncio
 async def test_real_response():
     s = Service()
@@ -12,13 +22,7 @@ async def test_real_response():
     assert r != "127.0.0.1"
 
 @pytest.mark.asyncio
-async def test_mock_response(aresponses):
-    aresponses.add(
-        "httpbin.org",
-        "/get",
-        "GET",
-        response=aiohttp.web.json_response({"origin":"127.0.0.1"}) # ! SIC !
-        )
+async def test_mock_response(mock_multiple_responses):
     s = Service()
     r = await s.fetch()
     assert r == "127.0.0.1"
